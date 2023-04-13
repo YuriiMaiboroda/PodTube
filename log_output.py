4024f17a51034b738d6244c5ec7a6ae1bc8841c7
YuriiMaiboroda <yurii.maiboroda@gmail.com>
2023-04-14 00:27:00 +0300

    improve log styling
    replace `<`, `>`, `@` and `=` characters to html codes
    use re.DOTALL flag
    fix BLOCK_END position
    use BLOCK_END for find block end
    more specific number ip, date and time formats
    add highlight hex and binary numbers
    ignore numbers in html like codes (&#\d+;)

--- log_output.py
+++ log_output.py
@@ -16,9 +16,12 @@ class LogFileHandler(StaticFileNonCacheHandler):
     BLOCK_START = "@@@___@@@___@@@"
     BLOCK_END = "===___===___==="
 
+    def escape(string: str) -> str:
+        return string.replace("<", "&lt;").replace(">", "&gt;").replace("@", "&#64;").replace("=", "&#61;")
+
     def add_block_borders(string: str) -> str:
-        string = re.sub(r"^\s*(\d+-\d+-\d+ \d+:\d+:\d+).*", rf"{LogFileHandler.BLOCK_START}\g<0>", string, flags=re.MULTILINE)
-        string = re.sub(rf"^{LogFileHandler.BLOCK_START}(.|\n)*?(?=\n{LogFileHandler.BLOCK_START}|\Z)", rf"\g<0>{LogFileHandler.BLOCK_END}", string, flags=re.MULTILINE)
+        string = re.sub(r"^[ \t]*(\d+-\d+-\d+ \d+:\d+:\d+)", rf"{LogFileHandler.BLOCK_START}\g<0>", string, flags = re.MULTILINE)
+        string = re.sub(rf"^{LogFileHandler.BLOCK_START}.*?(?=\n*(?:{LogFileHandler.BLOCK_START}|\Z))", rf"\g<0>{LogFileHandler.BLOCK_END}", string, flags = re.MULTILINE | re.DOTALL)
         return string
 
     def remove_block_borders(string: str) -> str:
@@ -33,29 +36,31 @@ class LogFileHandler(StaticFileNonCacheHandler):
                 return f"<span class='log-{match_obj.group(1).lower()}'>{match_obj.group()}</span>"
             return match_obj.group()
 
-        return re.sub(rf"^{LogFileHandler.BLOCK_START}.* \[(CRITICAL|ERROR|WARNING|INFO|DEBUG)\](.|\n)*?(?=\n{LogFileHandler.BLOCK_START}|\Z)", replacer, string, flags=re.MULTILINE)
+        return re.sub(rf"^{LogFileHandler.BLOCK_START}.*?\[(CRITICAL|ERROR|WARNING|INFO|DEBUG)\].*?{LogFileHandler.BLOCK_END}", replacer, string, flags = re.MULTILINE | re.DOTALL)
 
     def add_numbers_style(string: str):
         def replacer(match_obj: re.Match[str]):
+            if match_obj.group(5) is not None:
+                return match_obj.group()
             style_class = None
             if match_obj.group(1) is not None:
-                style_class = "log-date"
-            elif match_obj.group(2) is not None:
-                style_class = "log-time"
-            elif match_obj.group(3) is not None:
                 style_class = "log-ip"
+            elif match_obj.group(2) is not None:
+                style_class = "log-date"
+            elif match_obj.group(3) is not None:
+                style_class = "log-time"
             elif match_obj.group(4) is not None:
                 style_class = "log-number"
             if style_class is not None:
                 return "<span class='" + style_class + "'>" + match_obj.group() + "</span>"
             return match_obj.group()
-
-        return re.sub(r"(?:((?:\d+[-/])+\d+)|((?:\d+:)+\d+(?:,\d+)?)|((?:\d+\.)+\d+)|\b(\d+)\b)", replacer, string)
+        return re.sub(r"(?:((?:\d+\.){3}\d+)|((?:(?:\d+-){2}|(?:\d+/){1,2}|(?:\d+\.){2})\d+)|((?:\d+:){1,2}\d+(?:[,.]\d+)?)|(\b0(?:x[0-9a-fA-F]+|b[01]+)\b|(&#)?\b(\d+(?:[,.]\d+)?))(?(5);))", replacer, string)
 
     def add_string_style(string: str):
         return re.sub(r"(?:\"[^\"\n]*\")|(?:\'[^\'\n]*\')", r"<span class='log-string'>\g<0></span>", string)
 
     def log_style(string: str, add_br: bool = False) -> str:
+        string = LogFileHandler.escape(string)
         string = LogFileHandler.add_block_borders(string)
         string = LogFileHandler.add_string_style(string)
         string = LogFileHandler.add_numbers_style(string)
